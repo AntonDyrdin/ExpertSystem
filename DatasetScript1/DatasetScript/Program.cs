cd7dab4a0352ca4287895ea9f298d53caa4ef772
Anton <46337136+AntonDyrdin@users.noreply.github.com>
2019-01-03 22:14:32 +0300

    Add files via upload

---
+++ DatasetScript1/DatasetScript/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace DatasetScript
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var allLines = File.ReadAllLines(@"C:\Users\anton\Рабочий стол\MAIN\Временные ряды\timeSeries4.txt");
+            List<string> newLines = new List<string>();
+            foreach (string line in allLines)
+            {
+                if (line != "")
+                {
+                    var newline = line.Replace(',', ';');
+                    newLines.Add(newline.Replace('.', ','));
+                   // System.Console.WriteLine(newline);
+                }
+            }
+            File.WriteAllLines(@"C:\Users\anton\Рабочий стол\MAIN\Временные ряды\timeSeries4.txt", newLines);
+        }
+    }
+}
